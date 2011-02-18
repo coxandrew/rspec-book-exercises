0a95627b13b55b82da6cfe5439d221e8c72eb031
Andrew Cox <drew.cox@gmail.com>
2011-02-18 08:22:01 -0500

    Extract Marker class into its own file

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -15,32 +15,5 @@ module Codebreaker
       @output.puts '+'*marker.exact_match_count +
                    '-'*marker.number_match_count
     end
-
-    class Marker
-      def initialize(secret, guess)
-        @secret, @guess = secret, guess
-      end
-
-      def exact_match_count
-        (0..3).inject(0) do |count, index|
-          count + (exact_match?(index) ? 1 : 0)
-        end
-      end
-
-      def number_match_count
-        (0..3).inject(0) do |count, index|
-          count += (number_match?(index) ? 1 : 0)
-        end
-      end
-
-      def exact_match?(index)
-        @guess[index] == @secret[index]
-      end
-
-      def number_match?(index)
-        @secret.include?(@guess[index]) && !exact_match?(index)
-      end
-    end
-
   end
 end
