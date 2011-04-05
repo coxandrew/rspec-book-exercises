3bf55681c2537a9bbb447b274ed001ca2e72895b
Andrew Cox <drew.cox@gmail.com>
2011-04-05 08:44:02 -0400

    Example Rails app showtime with passing Cucumber scenario

---
+++ showtime/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showtime::Application.config.secret_token = '37cb62290af47116d3fa52d2be1146a7b22d29fc3456d4b10369a0a93cf54719795f5e72fdbe275d9b6b23579108bfaa26ae6b2001104abb15a125df66c7db40'
